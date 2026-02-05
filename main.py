b80556f55506bb3453314b0147e3361eaf35eb63
Izram Khan <izramk9@gmail.com>
2026-02-05 21:58:35 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# Create by: Izram Khan
+# Date completed: 5-Feb-2026
+# License: MIT
+#-----------------------------------------------------------------------------------------------------
+import guess_the_country
+
+if __name__ == '__main__':
+    guess_the_country.play_game()
